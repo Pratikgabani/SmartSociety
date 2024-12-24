ef40e7abe74eef38464e3947824e05f1e97c516a
Nisarg-dev <nisarg25june@gmail.com>
2024-12-24 18:35:07 +0530

    hello

---
+++ SocietyHub/src/components/Login/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useState } from 'react'
+function Login() {
+const [password,setpassword] = useState()
+const [blockNo,setblockNo] = useState(null)
+const [houseNo,sethouseNo] = useState(null)
+
+
+
+  return (
+    <div className='bg-white-300 text-blue-500'>
+      
+    </div>
+  )
+}
+
+export default Login
