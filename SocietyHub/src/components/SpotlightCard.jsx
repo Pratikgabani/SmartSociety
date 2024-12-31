a2dd95b594bc0b50df107c6288b6fffd8d5cf785
Nisarg-dev <nisarg25june@gmail.com>
2024-12-31 14:05:06 +0530

    hello

--- SocietyHub/src/components/SpotlightCard.jsx
+++ SocietyHub/src/components/SpotlightCard.jsx
@@ -1,6 +1,6 @@
 import { useRef, useState } from "react";
 
-const SpotlightCard = ({ children, className = "max-w-sm overflow-hidden", spotlightColor = "rgba(355, 355, 355, 0.25)" }) => {
+const SpotlightCard = ({ children, className = "max-w-sm overflow-hidden", spotlightColor = "rgba(32, 32, 32, 0.6)" }) => {
   const divRef = useRef(null);
   const [isFocused, setIsFocused] = useState(false);
   const [position, setPosition] = useState({ x: 0, y: 0 });
@@ -39,7 +39,7 @@ const SpotlightCard = ({ children, className = "max-w-sm overflow-hidden", spotl
       onBlur={handleBlur}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
-      className={`relative rounded-3xl border border-neutral-500 bg-neutral-600 overflow-hidden p-8 ${className}`}
+      className={`relative rounded-3xl border border-neutral-500 bg-blue-300 overflow-auto  p-8 ${className}`}
     >
       <div
         className="pointer-events-none absolute inset-0 opacity-0 transition-opacity duration-500 ease-in-out"
