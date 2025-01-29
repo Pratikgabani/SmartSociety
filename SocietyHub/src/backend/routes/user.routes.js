dcd75f31ccb06d50c449246e06ce6e3830d6e9b6
Nisarg-dev <nisarg25june@gmail.com>
2025-01-30 00:12:04 +0530

    hhjj

--- SocietyHub/src/backend/routes/user.routes.js
+++ SocietyHub/src/backend/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {User} from "../models/user.models.js";
-import {registerUser, loginUser , logoutUser , getUserDetail , changeCurrentPassword} from "../controllers/user.controllers.js";
+import {registerUser, loginUser , logoutUser , getUserDetail ,updateAccountDetails, changeCurrentPassword} from "../controllers/user.controllers.js";
 import {Router} from "express";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
@@ -11,4 +11,5 @@ router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT , logoutUser);
 router.route("/currentUser").get(verifyJWT , getUserDetail);
 router.route("/changePassword").post(verifyJWT , changeCurrentPassword);
+router.route("/updateAccountDetails").patch(verifyJWT , updateAccountDetails);
 export default router
