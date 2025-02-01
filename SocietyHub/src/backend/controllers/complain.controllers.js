d387f1a4af888768905c7496b9192275a656a275
Pratik <pratikgabani79@gmail.com>
2025-02-01 15:46:22 +0530

    complain routes and controlllers

--- SocietyHub/src/backend/controllers/complain.controllers.js
+++ SocietyHub/src/backend/controllers/complain.controllers.js
@@ -9,11 +9,11 @@ import {upload} from "./../middlewares/multer.middlewares.js"
 
 
 const createComplain = asyncHandler(async (req, res, next) => {
-    const { subject, description, date } = req.body;
+    const { subject, description } = req.body;
 
-    console.log(subject, description, date);
+    console.log(subject, description);
 
-    if ([subject, description, date].some((field) => field?.trim() === "")) {
+    if ([subject, description].some((field) => field?.trim() === "")) {
         throw new ApiError(400, "All fields are required");
     }
 
@@ -40,7 +40,7 @@ const createComplain = asyncHandler(async (req, res, next) => {
             complainId: req.user._id,
             subject,
             description,
-            date,
+            date : new Date().toLocaleDateString(),
             byHouse: req.user?.houseNo,
             proof: proof?.url || undefined // Only include if proof exists
         });
@@ -57,7 +57,7 @@ const createComplain = asyncHandler(async (req, res, next) => {
         if (proof) {
             await deleteFromCloudinary(proof.public_id);
         }
-        next(new ApiError(error.statusCode || 500, error.message || "Something went wrong"));
+        next(new ApiError(500, "Something went wrong"));
     }
 });
 
@@ -92,4 +92,33 @@ const deleteComplain = asyncHandler(async (req, res) => {
 
 });
 
-export { createComplain , deleteComplain , getAllComplains }
+const toggleComplain = asyncHandler(async (req, res) => {
+
+    const { complainId } = req.params;
+
+    if (!complainId) {
+        throw new ApiError(400, "Complain ID is required");
+    }
+
+    const complain = await Complain.findById(complainId);
+
+    if (!complain) {
+        throw new ApiError(404, "Complain not found");
+    }
+
+    const updatedComplain = await Complain.findByIdAndUpdate(complainId, {
+         isResolved: !complain.isResolved
+         }, { 
+            new: true 
+        });
+
+    if (!updatedComplain) {
+        throw new ApiError(500, "Failed to toggle complain");
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, updatedComplain, `Complain ${updatedComplain.isResolved ? "resolved" : "unresolved"} successfully`));
+})
+
+export { createComplain , deleteComplain , getAllComplains  , toggleComplain}
--- SocietyHub/src/backend/routes/complain.routes.js
+++ SocietyHub/src/backend/routes/complain.routes.js
@@ -1,11 +1,12 @@
 import {Router} from "express";
-import { createComplain, deleteComplain, getAllComplains } from "../controllers/complain.controllers.js";
+import { createComplain, deleteComplain, getAllComplains , toggleComplain} from "../controllers/complain.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 import {upload} from "./../middlewares/multer.middlewares.js"
 const router = Router();
 
 router.route("/createComplain").post(verifyJWT, upload.single("proof"), createComplain);
-router.route("/delete").delete(verifyJWT , deleteComplain);
+router.route("/deleteComplain/:complainId").delete(verifyJWT , deleteComplain);
 router.route("/getAllComplains").get(verifyJWT , getAllComplains);
+router.route("/toggleComplain/:complainId").patch(verifyJWT , toggleComplain);
 
 export default router
