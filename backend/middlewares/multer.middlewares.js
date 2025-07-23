2b86de6476b949927473ba5933ad4746e7a7d24f
Nisarg-dev <nisarg25june@gmail.com>
2025-07-23 12:34:46 +0530

    temp

--- backend/middlewares/multer.middlewares.js
+++ backend/middlewares/multer.middlewares.js
@@ -2,7 +2,7 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, "./public")
+      cb(null, "./public/temp") // Ensure this directory exists
     },
     filename: function (req, file, cb) {
       
